fb05ef6f43c48122face3980a812eb047dd0d8f2
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2026-07-27 19:39:33 +0200

    Add class `basicV20` and its `__init__` method

--- VTT/sheets/V20.py
+++ VTT/sheets/V20.py
@@ -10,3 +10,42 @@
 # Normal comment
 #// Deprecated code
 #*===============================================================================================================================
+
+#* IMPORTS
+
+#* CLASS "basicV20"
+class basicV20:
+    #& Documentation
+    #* METHODS
+    # __init__
+    def __init__(
+        self,
+        name:str,player:str,chronicle:str,nature:str,demeanor:str,concept:str,clan:str,generation:int,sire:str,
+        attributes:dict[str,int],skills:dict[str,int],
+        disciplines:dict[str,int],backgrounds:dict[str,int],virtues:dict[str,int],
+        merits:dict[str,int],flaws:dict[str,int],
+        path:dict[str,int],willpower:dict[str,int],blood_pool:int,
+        health:int,
+        weakness:str,
+        experience:int
+    ):
+        # Declare instance attributes for the elements of the basic V20 character sheet
+        (
+            self.name,self.player,self.chronicle,self.nature,self.demeanor,self.concept,self.clan,self.generation,self.sire,
+            self.attributes,self.skills,
+            self.disciplines,self.backgrounds,self.virtues,
+            self.merits,self.flaws,
+            self.path,self.willpower,self.blood,
+            self.health,
+            self.weakness,
+            self.experience
+        )=(
+            name,player,chronicle,nature,demeanor,concept,clan,generation,sire,
+            attributes,skills,
+            disciplines,backgrounds,virtues,
+            merits,flaws,
+            path,willpower,blood_pool,
+            health,
+            weakness,
+            experience
+        )
